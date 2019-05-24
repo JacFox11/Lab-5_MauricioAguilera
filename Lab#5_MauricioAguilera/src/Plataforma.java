8a19d3c820f36580338b0f066bf26091fba9e68a
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-05-24 17:59:41 -0600

    update

--- Lab#5_MauricioAguilera/src/Plataforma.java
+++ Lab#5_MauricioAguilera/src/Plataforma.java
@@ -83,6 +83,22 @@ public class Plataforma extends javax.swing.JFrame {
         popup_arbol = new javax.swing.JPopupMenu();
         ji_modificar = new javax.swing.JMenuItem();
         ji_eliminar = new javax.swing.JMenuItem();
+        jd_modestudiante = new javax.swing.JDialog();
+        jLabel11 = new javax.swing.JLabel();
+        tf_nombreestu1 = new javax.swing.JTextField();
+        jLabel12 = new javax.swing.JLabel();
+        tf_apellidoestu1 = new javax.swing.JTextField();
+        jLabel13 = new javax.swing.JLabel();
+        ff_cuenta1 = new javax.swing.JFormattedTextField();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        rb_m1 = new javax.swing.JRadioButton();
+        rb_f1 = new javax.swing.JRadioButton();
+        sp_edadestu1 = new javax.swing.JSpinner();
+        cb_carreras1 = new javax.swing.JComboBox<>();
+        jLabel16 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        buttonGroup2 = new javax.swing.ButtonGroup();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -300,6 +316,11 @@ public class Plataforma extends javax.swing.JFrame {
         );
 
         ji_modificar.setText("Modificar");
+        ji_modificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ji_modificarActionPerformed(evt);
+            }
+        });
         popup_arbol.add(ji_modificar);
 
         ji_eliminar.setText("Eliminar");
@@ -309,6 +330,115 @@ public class Plataforma extends javax.swing.JFrame {
             }
         });
         popup_arbol.add(ji_eliminar);
+
+        jd_modestudiante.setTitle("Modificar");
+
+        jLabel11.setText("Nombre");
+
+        jLabel12.setText("Apellido");
+
+        jLabel13.setText("Num. de Cuenta");
+
+        try {
+            ff_cuenta1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("########")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        jLabel14.setText("Edad");
+
+        jLabel15.setText("Genero");
+
+        buttonGroup2.add(rb_m1);
+        rb_m1.setSelected(true);
+        rb_m1.setText("M");
+
+        buttonGroup2.add(rb_f1);
+        rb_f1.setText("F");
+
+        cb_carreras1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Finanzas", "Derecho", "Administración de Empresas", "Diseño Gráfico", "Mercadotecnia", "Sistemas Computacionales", "Industrial y de Sistemas", "Mecatrónica", "Biomédica", "Energía", "Medicina y Cirugia", "Odontologia", "Nutricion", "Terapia Física y Ocupacional" }));
+
+        jLabel16.setText("Carrera");
+
+        jButton2.setText("Modificar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jd_modestudianteLayout = new javax.swing.GroupLayout(jd_modestudiante.getContentPane());
+        jd_modestudiante.getContentPane().setLayout(jd_modestudianteLayout);
+        jd_modestudianteLayout.setHorizontalGroup(
+            jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_modestudianteLayout.createSequentialGroup()
+                .addContainerGap(339, Short.MAX_VALUE)
+                .addComponent(jLabel16)
+                .addGap(50, 50, 50)
+                .addComponent(cb_carreras1, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(94, 94, 94))
+            .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jd_modestudianteLayout.createSequentialGroup()
+                    .addGap(51, 51, 51)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jd_modestudianteLayout.createSequentialGroup()
+                            .addGap(132, 132, 132)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 317, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jd_modestudianteLayout.createSequentialGroup()
+                            .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel13)
+                                .addComponent(jLabel14)
+                                .addComponent(jLabel15))
+                            .addGap(23, 23, 23)
+                            .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(tf_nombreestu1)
+                                .addComponent(tf_apellidoestu1)
+                                .addComponent(ff_cuenta1)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_modestudianteLayout.createSequentialGroup()
+                                    .addComponent(rb_m1)
+                                    .addGap(31, 31, 31)
+                                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(rb_f1)
+                                        .addComponent(sp_edadestu1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                    .addContainerGap(211, Short.MAX_VALUE)))
+        );
+        jd_modestudianteLayout.setVerticalGroup(
+            jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jd_modestudianteLayout.createSequentialGroup()
+                .addGap(109, 109, 109)
+                .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cb_carreras1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel16))
+                .addContainerGap(266, Short.MAX_VALUE))
+            .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jd_modestudianteLayout.createSequentialGroup()
+                    .addGap(82, 82, 82)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel11)
+                        .addComponent(tf_nombreestu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(24, 24, 24)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(tf_apellidoestu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel12))
+                    .addGap(24, 24, 24)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel13)
+                        .addComponent(ff_cuenta1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(24, 24, 24)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel14)
+                        .addComponent(sp_edadestu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(22, 22, 22)
+                    .addGroup(jd_modestudianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel15)
+                        .addComponent(rb_f1)
+                        .addComponent(rb_m1))
+                    .addGap(18, 18, 18)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(36, Short.MAX_VALUE)))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -457,7 +587,7 @@ public class Plataforma extends javax.swing.JFrame {
                     getLastPathComponent();
             nodo_seleccionado = (DefaultMutableTreeNode) v1;
             if (nodo_seleccionado.getUserObject() instanceof Estudiante) {
-                estudiante_seleccionada
+                estudiante_seleccionado
                         = (Estudiante) nodo_seleccionado.
                         getUserObject();
                 popup_arbol.show(evt.getComponent(),
@@ -474,6 +604,58 @@ public class Plataforma extends javax.swing.JFrame {
             m.removeNodeFromParent(nodo_seleccionado);
             m.reload();
     }//GEN-LAST:event_ji_eliminarActionPerformed
+
+    private void ji_modificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ji_modificarActionPerformed
+        // TODO add your handling code here:
+        Estudiante estudiante = (Estudiante) nodo_seleccionado.getUserObject();
+            tf_nombreestu1.setText(estudiante.getNombre());
+            tf_apellidoestu1.setText(estudiante.getApellido());
+            ff_cuenta1.setText(estudiante.getCuenta());
+            sp_edadestu1.setValue(estudiante.getEdad());
+        jd_modestudiante.setModal(true);
+        jd_modestudiante.pack();
+        jd_modestudiante.setVisible(true);
+            
+    }//GEN-LAST:event_ji_modificarActionPerformed
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+        try{
+            
+            String nombre = tf_nombreestu1.getText();
+            String apellido = tf_apellidoestu1.getText();
+            String cuenta = ff_cuenta1.getText();
+            int edad = (Integer)sp_edadestu1.getValue();
+            String sexo="";
+            if (rb_m1.isSelected()){
+                sexo="Maculino";
+            }
+            else{
+                sexo="Femenino";
+            }
+            Carrera carrera= new Carrera();
+            for (int i = 0; i < 14; i++) {
+                for (int j = 0; j < carreras.size(); j++) {
+                    if ((String)cb_carreras1.getSelectedItem() == carreras.get(j).getNombre()){
+                        carrera = carreras.get(j);
+                    }
+                }
+            }
+            DefaultTreeModel m = (DefaultTreeModel) jt_estudiantes.getModel();
+            estudiante_seleccionado = new Estudiante(nombre, apellido, sexo, cuenta, edad, carrera);
+            m.reload();
+            JOptionPane.showMessageDialog(jd_modestudiante, "Modificacion exitosa");
+            jd_modestudiante.setVisible(false);
+            tf_nombreestu1.setText("");
+            tf_apellidoestu1.setText("");
+            ff_cuenta1.setText("");
+            sp_edadestu1.setValue(20);
+            cb_carreras1.setSelectedIndex(0);
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(jd_estudiante, "No se pudo agregar el nuevo alumno ya que uno de los valores es invalido");
+        }
+    }//GEN-LAST:event_jButton2MouseClicked
 
     /**
      * @param args the command line arguments
@@ -512,11 +694,21 @@ public class Plataforma extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.JComboBox<String> cb_carreras;
+    private javax.swing.JComboBox<String> cb_carreras1;
     private javax.swing.JFormattedTextField ff_cuenta;
+    private javax.swing.JFormattedTextField ff_cuenta1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -536,6 +728,7 @@ public class Plataforma extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JDialog jd_estudiante;
     private javax.swing.JDialog jd_maestros;
+    private javax.swing.JDialog jd_modestudiante;
     private javax.swing.JMenuItem ji_eliminar;
     private javax.swing.JMenuItem ji_modificar;
     private javax.swing.JList<String> jl_estudiantes;
@@ -543,18 +736,23 @@ public class Plataforma extends javax.swing.JFrame {
     private javax.swing.JPopupMenu popup_arbol;
     private javax.swing.JPopupMenu popup_estudiante;
     private javax.swing.JRadioButton rb_f;
+    private javax.swing.JRadioButton rb_f1;
     private javax.swing.JRadioButton rb_m;
+    private javax.swing.JRadioButton rb_m1;
     private javax.swing.JSpinner sp_edadestu;
+    private javax.swing.JSpinner sp_edadestu1;
     private javax.swing.JSpinner sp_edadmaes;
     private javax.swing.JSpinner sp_salario;
     private javax.swing.JTextField tf_apellidoestu;
+    private javax.swing.JTextField tf_apellidoestu1;
     private javax.swing.JTextField tf_apellidomaes;
     private javax.swing.JTextField tf_nombreestu;
+    private javax.swing.JTextField tf_nombreestu1;
     private javax.swing.JTextField tf_nombremaes;
     // End of variables declaration//GEN-END:variables
 
 ArrayList <Maestro> maestros = new ArrayList();
 ArrayList <Clase> clases = new ArrayList();
 DefaultMutableTreeNode nodo_seleccionado;
-Estudiante estudiante_seleccionada;
+Estudiante estudiante_seleccionado;
 }
